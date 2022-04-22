d2208a044f9758146b219273be00fc459da548bc
Kevin Koshy <kevin999koshy@gmail.com>
2022-04-22 11:53:53 +0530

    Add logic in EJS views

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use("view-engine", "ejs");
+app.set("view engine", "ejs");
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
@@ -16,13 +16,37 @@ app.get("/", function(req, res){
     var currentDay =today.getDay();
     var day = "";
 
-    if (currentDay ===6 || currentDay===0){
-        day = "weekend";
-        res.render("list",{kindOfDay:day});
-    }
-    else{
-        day ="weekday";
-        res.render("list",{kindOfDay:day});
+    // if (currentDay ===6 || currentDay===0){
+    //     day = "weekend";
+    // }
+    // else{
+    //     day ="weekday";
+    // }
+
+    switch (currentDay)
+    {
+        case 0:
+            day="Sunday";
+            break;
+        case 1:
+            day="Monday";
+            break;
+        case 2:
+            day="Tuesday";
+            break;
+        case 3:
+            day="Wednesday";
+            break;
+        case 4:
+            day="Thursday";
+            break;
+        case 5:
+            day="Friday";
+            break;
+        case 6:
+            day="Saturday";
+            break;
     }
 
+        res.render("list",{kindOfDay:day});
 });
