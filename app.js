365efa724f12632fef9aea3f1d3fb4ee20037854
Kevin Koshy <kevin999koshy@gmail.com>
2022-04-22 18:03:06 +0530

    add css styles

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const bodyParser = require("body-parser");
 
 const app =  express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"))
 
 app.set("view engine", "ejs");
 
-var items =["Buy Food", "Cook Food", "Eat Food"];
+let items =["Buy Food", "Cook Food", "Eat Food"];
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
@@ -14,21 +15,21 @@ app.listen(3000, function(){
 
 app.get("/", function(req, res){
 
-    var today = new Date();
-    var currentDay =today.getDay();
-    var options = {
+    let today = new Date();
+    // let currentDay =today.getDay();
+    let options = {
         weekday:"long",
         day:"numeric",
         month:"long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
     res.render("list",{kindOfDay:day, newListItems:items});
 
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item);
     res.redirect("/");
 })
