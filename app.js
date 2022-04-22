946e644bab3cf114d7aeea4841edbf233911d2d9
Kevin Koshy <kevin999koshy@gmail.com>
2022-04-22 10:50:28 +0530

    Add EJS functionality

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use("view-engine", "ejs");
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname+"/index.html");
+
+    var today = new Date();
+    var currentDay =today.getDay();
+    var day = "";
+
+    if (currentDay ===6 || currentDay===0){
+        day = "weekend";
+        res.render("list",{kindOfDay:day});
+    }
+    else{
+        day ="weekday";
+        res.render("list",{kindOfDay:day});
+    }
+
 });
