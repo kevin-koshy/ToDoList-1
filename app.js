5dc1ec550387cfad8d9ed798e6940302423bc23f
Kevin Koshy <kevin999koshy@gmail.com>
2022-04-23 17:46:04 +0530

    Add date functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
 
 const app =  express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -8,6 +9,7 @@ app.use(express.static("public"))
 app.set("view engine", "ejs");
 
 let items =["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
@@ -15,21 +17,35 @@ app.listen(3000, function(){
 
 app.get("/", function(req, res){
 
-    let today = new Date();
-    // let currentDay =today.getDay();
-    let options = {
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-    res.render("list",{kindOfDay:day, newListItems:items});
+    let day = date();
+    res.render("list",{listTitle:day, newListItems:items});
 
 });
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle:"Work", newListItems:workItems})
+})
+
+app.get("/about", function(req, res){
+    res.render("about");
+})
+
+app.post("/work", function(req, res){
+    // let item = req.body.newItem;
+    // workItems.push(item);
+    // res.redirect("/")
+})
+
+
 app.post("/", function(req, res){
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    console.log(req.body.list);
+    if (req.body.list ==="Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
 })
